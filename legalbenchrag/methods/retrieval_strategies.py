1ee9d0fbe040aa53e44b1996f97eca325ab1618d
KingEsel1 <reutermarkus1@gmail.com>
2025-04-23 13:54:49 +0200

    params

--- legalbenchrag/methods/retrieval_strategies.py
+++ legalbenchrag/methods/retrieval_strategies.py
@@ -9,7 +9,7 @@ from legalbenchrag.methods.hypa import HypaStrategy
 # Define common chunking strategies for baseline
 chunk_strategies: List[BaselineChunkingStrategy] = [
     BaselineChunkingStrategy(strategy_name="rcts", chunk_size=500, chunk_overlap_ratio=0.0),
-    BaselineChunkingStrategy(strategy_name="naive", chunk_size=500),
+    # BaselineChunkingStrategy(strategy_name="naive", chunk_size=500),
 ]
 
 # Define common embedding models
@@ -23,7 +23,7 @@ baseline_embed_strategies = [
     # hf_embed_model_bge,
     # oai_embed_model,
     hf_embed_model_legalbert_base,
-    hf_embed_model_legalbert_small,
+    # hf_embed_model_legalbert_small,
 ]
 
 # Define rerank models
@@ -33,7 +33,7 @@ rerank_models: list[AIRerankModel | None] = [
 ]
 
 # Define top_k values
-top_ks: list[int] = [1, 2, 4, 8, 16, 32, 64]
+top_ks: list[int] = [1, 2]  # , 4, 8, 16, 32, 64]
 
 BASELINE_STRATEGIES: list[BaselineStrategy] = []
 for chunk_strategy in chunk_strategies:
@@ -86,7 +86,7 @@ for chunk_strategy in chunk_strategies:
 # Use Union for type hinting the list
 ALL_RETRIEVAL_STRATEGIES: List[Union[BaselineStrategy, HypaStrategy]] = []
 ALL_RETRIEVAL_STRATEGIES.extend(BASELINE_STRATEGIES)
-ALL_RETRIEVAL_STRATEGIES.extend(HYPA_STRATEGIES)
+# ALL_RETRIEVAL_STRATEGIES.extend(HYPA_STRATEGIES)
 
 print(f"Defined {len(BASELINE_STRATEGIES)} Baseline strategies.")
 print(f"Defined {len(HYPA_STRATEGIES)} HyPA strategies.")
